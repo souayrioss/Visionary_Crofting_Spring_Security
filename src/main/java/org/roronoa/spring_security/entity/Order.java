c1c6af7d237931d4695a88393bccee5d5578c035
Oussama Souayri <souayri.oussama@gmail.com>
2022-12-19 21:00:05 +0100

    use spring data for linked data base

--- src/main/java/org/roronoa/spring_security/entity/Order.java
+++ src/main/java/org/roronoa/spring_security/entity/Order.java
@@ -1,12 +1,13 @@
 package org.roronoa.spring_security.entity;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonSetter;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -16,19 +17,24 @@ import java.util.Set;
 import java.util.UUID;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "customer_order")
 public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
+    @NotEmpty @NotBlank
     private UUID uuid;
-
     private LocalDateTime createdAt;
 
+    @NotEmpty @NotNull
     private Double totalPrice;
 
-    @OneToMany(mappedBy = "order") @NotNull @NotEmpty @Valid
+    @NotNull @NotEmpty @Valid
+    @OneToMany(mappedBy = "order")
     private Set<OrderItem> orderItems = new HashSet<>();
 
     public Order(UUID uuid, LocalDateTime createdAt, Double totalPrice, @NotNull @NotEmpty Set<OrderItem> orderItems) {
@@ -38,47 +44,4 @@ public class Order implements Serializable {
         this.orderItems = orderItems;
     }
 
-    public Order() {
-    }
-
-    public UUID getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(UUID uuid) {
-        this.uuid = uuid;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Double gettotalPrice() {
-        return totalPrice;
-    }
-
-    public void settotalPrice(Double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    @JsonIgnore
-    public Set<OrderItem> getOrderItems() {
-        return orderItems;
-    }
-    @JsonSetter
-    public void setOrderItems (Set<OrderItem> orderItems) {
-        this.orderItems = orderItems;
-    }
 }
--- src/main/java/org/roronoa/spring_security/entity/OrderItem.java
+++ src/main/java/org/roronoa/spring_security/entity/OrderItem.java
@@ -1,78 +1,41 @@
 package org.roronoa.spring_security.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class OrderItem implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-    @NotNull @NotEmpty
-    private String refProduct;
+    @NotNull @NotEmpty @Valid
+    @ManyToOne
+    private Product product;
     @NotNull
     private Integer quantity;
     @NotNull(message = "price is mandatory")
     private Double price;
 
-    @ManyToOne
+    @ManyToOne @Valid @NotNull
     private Order order;
 
-    public OrderItem() {
-    }
 
-    public OrderItem(String refProduct, Integer quantity, Double price, Order order) {
-        this.refProduct = refProduct;
-        this.quantity = quantity;
-        this.price = price;
-        this.order = order;
-    }
-
-    public String getRefProduct() {
-        return refProduct;
-    }
-
-    public void setRefProduct(String refProduct) {
-        this.refProduct = refProduct;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public void setOrder(Order order) {
-        this.order = order;
-    }
 }
