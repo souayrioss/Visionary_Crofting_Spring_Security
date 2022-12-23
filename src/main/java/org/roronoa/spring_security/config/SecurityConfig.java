76d4f6f64eb60fcd732eca236bdb77702801cda8
Oussama Souayri <souayri.oussama@gmail.com>
2022-12-23 10:28:25 +0100

    add Security Configuration

---
+++ src/main/java/org/roronoa/spring_security/config/SecurityConfig.java
@@ -0,0 +1,76 @@
+package org.roronoa.spring_security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.roronoa.spring_security.service.IUserService;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configurable
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    IUserService userService;
+
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/login").permitAll()
+                .antMatchers("/api/v1/user/register").hasAuthority("client")
+                .antMatchers(HttpMethod.POST,"/api/v1/product/**").hasAnyAuthority("stock","supplier")
+                .antMatchers("/api/v1/appelOffre/add").hasAuthority("stock")
+                .antMatchers("/api/v1/commande/**").hasAuthority("client")
+                .antMatchers(HttpMethod.GET,"/api/v1/product/**").hasAuthority("client")
+                .antMatchers("/api/v1/appelOffre/**").hasAuthority("supplier")
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(STATELESS)
+                .and()
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return email -> userService.findByEmail(email);
+
+
+    }
+
+}
